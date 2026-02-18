e92b5db5df9b6ef25288a1dfc07b72eb1c449029
asrafulhoque25 <asrafulhoqueemon25@gmail.com>
2026-02-18 12:41:30 +0600

    scroll animation added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1485,3 +1485,31 @@ if (document.readyState === 'loading') {
 } else {
     initBlogDetails();
 }
+
+
+
+
+
+
+
+//smooth scroll
+
+// Initialize Lenis
+const lenis = new Lenis({
+  duration: 1.2,     
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), 
+  direction: 'vertical', 
+  gestureDirection: 'vertical',
+  smoothWheel: true,
+  wheelMultiplier: 1.1, 
+  infinite: false,
+});
+
+
+lenis.on('scroll', ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
